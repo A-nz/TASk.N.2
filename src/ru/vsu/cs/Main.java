8c0ae9ce3e00002bff4c9ced05d7ffc63537aa48
Petrov Andrey <pandr2002@mail.ru>
2020-10-22 09:50:08 +0300

    .

--- src/ru/vsu/cs/Main.java
+++ src/ru/vsu/cs/Main.java
@@ -6,21 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Point p1 = ReadPoint1("p1");
-        Point p2 = ReadPoint1("p2");
-        Point p3 = ReadPoint1("p3");
-        Point p4 = ReadPoint1("p4");
-        System.out.print(sravnenieStoron(p1, p2, p3, p4));
+        Point p1 = readPoint1("p1");
+        Point p2 = readPoint1("p2");
+        Point p3 = readPoint1("p3");
+        Point p4 = readPoint1("p4");
+        System.out.print(defineSquare(p1, p2, p3, p4));
     }
 
-    private static Point ReadPoint1(String name) {
+    private static Point readPoint1(String name) {
 
-        double x = ReadDouble(name + ".x");
-        double y = ReadDouble(name + ".y");
+        double x = readDouble(name + ".x");
+        double y = readDouble(name + ".y");
         return new Point(x, y);
     }
 
-    public static double ReadDouble(String name) {
+    private static double readDouble(String name) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter %s: ", name);
@@ -28,19 +28,17 @@ public class Main {
     }
 
 
-    public static boolean sravnenieStoron (Point p1, Point p2, Point p3, Point p4)
+    private static boolean defineSquare (Point p1, Point p2, Point p3, Point p4)
     {
-        return     (FoundDistance(p1, p2) == FoundDistance(p2, p3))
-                && (FoundDistance(p2, p3) == FoundDistance(p3, p4))
-                && (FoundDistance(p3, p4) == FoundDistance(p4, p1))
-                && (FoundDistance(p4, p1) == FoundDistance(p1, p2))
+        return     (foundDistance(p1, p2) == foundDistance(p2, p3))
+                && (foundDistance(p2, p3) == foundDistance(p3, p4))
+                && (foundDistance(p3, p4) == foundDistance(p4, p1))
+                && (foundDistance(p4, p1) == foundDistance(p1, p2))
 
-                && (FoundDistance(p1, p3) == FoundDistance(p2, p4));
-
-
+                && (foundDistance(p1, p3) == foundDistance(p2, p4));
     }
 
-    public static double FoundDistance(Point p1, Point p2)
+    private static double foundDistance(Point p1, Point p2)
     {
         return  Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
     }
